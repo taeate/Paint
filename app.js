7ecaa3291af77d3d59b96f4a187850fb3ed6d69a
성진님 <seongjin@seongjinnim-ui-MacBookAir.local>
2022-10-05 16:56:44 +0900

    beginPath 로 경로 절단 새로운 선

--- app.js
+++ app.js
@@ -5,4 +5,13 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(50, 50, 100, 300);
+ctx.rect(50, 50, 100, 100);
+ctx.rect(150, 150, 100, 100);
+ctx.rect(250, 250, 100, 100);
+ctx.fill();
+
+ctx.beginPath();
+ctx.rect(350, 350, 100, 100);
+ctx.fillStyle = "red";
+setTimeout(() => {ctx.fill();}, 3000);
+
