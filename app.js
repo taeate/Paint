583214d1f669811a54db71222af00e1c0f717ef8
taeate <yes3144@nate.com>
2022-10-05 17:34:11 +0900

    arc use 동그라미 만들기

--- app.js
+++ app.js
@@ -5,16 +5,17 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 50, 200);
-ctx.fillRect(400, 200, 50, 200);
-ctx.lineWidth = 2;
-ctx.fillRect(300, 300, 50, 100);
-ctx.fillRect(200, 200, 200, 30);
-ctx.moveTo(200, 200);
-ctx.lineTo(325, 100);
-ctx.lineTo(450, 200);
+ctx.fillRect(200, 200, 15, 100);
+ctx.fillRect(360, 200, 15, 100);
+ctx.fillRect(260, 200, 60, 200);
+
+ctx.arc(288, 120, 50, 0, 2 * Math.PI);
 ctx.fill();
 
-
+ctx.beginPath();
+ctx.fillStyle = "white";
+ctx.arc(270, 120, 5, 0, 2 * Math.PI);
+ctx.arc(305, 120, 5, 0, 2 * Math.PI);
+ctx.fill();
 
 
