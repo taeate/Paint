96636f92f727f60af091c0b70f260ca66244ebdc
taeate <yes3144@nate.com>
2022-10-05 17:44:49 +0900

    mousemove 함수로 그림그리기

--- app.js
+++ app.js
@@ -5,17 +5,14 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.fillRect(200, 200, 15, 100);
-ctx.fillRect(360, 200, 15, 100);
-ctx.fillRect(260, 200, 60, 200);
+ctx.lineWidth = 2;
+ctx.moveTo(0,0);
 
-ctx.arc(288, 120, 50, 0, 2 * Math.PI);
-ctx.fill();
 
-ctx.beginPath();
-ctx.fillStyle = "white";
-ctx.arc(270, 120, 5, 0, 2 * Math.PI);
-ctx.arc(305, 120, 5, 0, 2 * Math.PI);
-ctx.fill();
+function onClick(event) {
+    ctx.lineTo(event.offsetX, event.offsetY);
+    ctx.stroke();
+}
 
+canvas.addEventListener('mousemove', onClick);
 
