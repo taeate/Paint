0b98d11613e295c915b19b53c8082bd3eaf19289
taeate <yes3144@nate.com>
2022-10-06 15:41:41 +0900

    change event 로 선 굵기 선택

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const canvas = document.querySelector('canvas');
-
+const lineWidth = document.getElementById('line-width');
 const ctx = canvas.getContext('2d');
 
 canvas.width = 800;
 canvas.height = 800;
 
-ctx.lineWidth = 2;
+ctx.lineWidth = lineWidth.value;
 ctx.moveTo(0,0);
 let isPainting = false;
 
@@ -15,6 +15,7 @@ function onMove(event) {
         ctx.stroke();
         return;
     }
+    ctx.beginPath();
     ctx.moveTo(event.offsetX, event.offsetY);
 }
 function onMouseDown() {
@@ -24,7 +25,15 @@ function onMouseUp() {
     isPainting = false;
  }
 
+
+function onLineWidthChange(event){
+    ctx.lineWidth = event.target.value;
+}
+
 canvas.addEventListener('mousemove', onMove);
 canvas.addEventListener('mousedown', onMouseDown);
 canvas.addEventListener('mouseup', onMouseUp);
 canvas.addEventListener('mouseleave', onMouseUp);
+
+
+lineWidth.addEventListener('change', onLineWidthChange);
