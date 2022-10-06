da6ff255fe1d79f034ceb3af55d7b79f424f6bc2
taeate <yes3144@nate.com>
2022-10-06 16:48:31 +0900

    fill 버튼과 Draw 버튼을 만들어서 배경색채우기

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector('canvas');
 const lineWidth = document.getElementById('line-width');
 const color = document.getElementById('color');
+const modeBtn = document.getElementById('mode-btn');
 const colorOptions = Array.from(document.getElementsByClassName('color-option'));
 const ctx = canvas.getContext('2d');
 
@@ -10,6 +11,7 @@ canvas.height = 800;
 ctx.lineWidth = lineWidth.value;
 ctx.moveTo(0,0);
 let isPainting = false;
+let isFilling = false;
 
 function onMove(event) {
     if(isPainting){
@@ -39,19 +41,38 @@ function onColorChange(event){
 
 function onColorClick(event){
     const colorData = event.target.dataset.color;
-    
+
     ctx.strokeStyle = colorData;
     ctx.fillStyle = colorData;
     color.value = colorData;
+}
+
+function onModeClick(){
+    if(isFilling){
+        isFilling = false;
+        modeBtn.innerText = "Fill";
+    }else{
+        isFilling = true;
+        modeBtn.innerText = "Draw";
+    }
+}
+
+function onCanvasClick(){
+    if(isFilling){
+        ctx.fillRect(0,0,800,800);
+    }
 }
 
 canvas.addEventListener('mousemove', onMove);
 canvas.addEventListener('mousedown', onMouseDown);
 canvas.addEventListener('mouseup', onMouseUp);
 canvas.addEventListener('mouseleave', onMouseUp);
+canvas.addEventListener('click', onCanvasClick);
 
 
 lineWidth.addEventListener('change', onLineWidthChange);
 color.addEventListener('change', onColorChange);
 
 colorOptions.forEach(color => color.addEventListener('click', onColorClick));
+
+modeBtn.addEventListener('click', onModeClick);
